f666cb2c3951df901ca10944de47f505ad492f40
briannkhata <briannkhata@gmail.com>
2023-04-01 09:43:04 +0200

    hkeeping of day2

---
+++ day2/VueCounterApp/src/App.vue
@@ -0,0 +1,35 @@
+    
+<script setup>
+
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<template>
+  <main>
+    <div>
+      <h4>The current count</h4>
+      <h1>{{ count }}</h1>
+      <button @click="count--">-</button>
+      <button @click="count++">+</button>
+    </div>
+  </main>
+</template>
+
+
+
+<style scoped>
+main {
+  width: 100vw;
+  height: 100vh;
+  background-color: aliceblue;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+div {
+  text-align: center;
+}
+</style>

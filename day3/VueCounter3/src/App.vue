320cf6bae847eea60f20a756b1cab17d0b9ff4f1
briannkhata <briannkhata@gmail.com>
2023-04-01 10:55:42 +0200

    hkeeping

---
+++ day3/VueCounter3/src/App.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { ref } from 'vue'
+let count = ref(0);
+
+const addToCount = () => {
+  count.value++;
+}
+
+const subtractFromCount = () => {
+  count.value--;
+}
+
+</script>
+
+<template>
+  <main>
+    <div>
+      <h4>Current count ...</h4>
+      <h1>{{ count }}</h1>
+      <button @click="subtractFromCount">-</button>
+      <button @click="addToCount">+</button>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+main {
+  background-color: aliceblue;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+div {
+  text-align: center;
+}
+</style>

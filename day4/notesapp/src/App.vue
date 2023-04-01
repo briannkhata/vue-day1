acb5b31cdecdac26b0949a1ea9dfd355db91448e
briannkhata <briannkhata@gmail.com>
2023-04-01 16:05:39 +0200

    display modal

--- day4/notesapp/src/App.vue
+++ day4/notesapp/src/App.vue
@@ -1,19 +1,49 @@
+<script setup>
+import { ref } from 'vue'
+
+const showModal = ref(false)
+
+</script>
+
+
 <template>
   <main>
+    <div v-if="showModal" class="overlay">
+      <div class="modal">
+        <textarea name="note" id="note" cols="30" rows="10"></textarea>
+        <button>Add Note</button>
+        <button @click="showModal = false" class="close">Close</button>
+      </div>
+    </div>
     <div class="container">
       <header>
         <h1>Notes</h1>
-        <button>+</button>
+        <button @click="showModal = true">+</button>
       </header>
       <div class="cards-container">
         <div class="card">
           <p class="main-text">
-
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id voluptatibus accusantium tempore recusandae nemo
+            mollitia facere quidem nobis quis culpa impedit, cupiditate debitis quae illum eligendi asperiores nihil vel.
+            Ducimus.1
           </p>
           <p class="date">
-
+            01/04/2023
           </p>
         </div>
+
+        <div class="card">
+          <p class="main-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id voluptatibus accusantium tempore recusandae nemo
+            mollitia facere quidem nobis quis culpa impedit, cupiditate debitis quae illum eligendi asperiores nihil vel.
+            Ducimus.1
+          </p>
+          <p class="date">
+            01/04/2022
+          </p>
+        </div>
+
+
       </div>
     </div>
   </main>
@@ -44,7 +74,7 @@ h1 {
   font-size: 75px;
 }
 
-button {
+header button {
   border: none;
   padding: 10px;
   width: 50px;
@@ -66,6 +96,55 @@ button {
   justify-content: space-between;
   margin-right: 20px;
   margin-bottom: 20px;
-  ;
+  border-radius: 5%;
+
+}
+
+.date {
+  font-size: 12.5px;
+  font-weight: bold;
+}
+
+.cards-container {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.overlay {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: rgb(0, 0, 0, 0.77);
+  z-index: 10;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.modal {
+  width: 750px;
+  background-color: white;
+  border-radius: 10px;
+  padding: 30px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+
+}
+
+.modal button {
+  padding: 10px 20px;
+  font-size: 20px;
+  width: 100%;
+  background-color: blueviolet;
+  border: none;
+  color: white;
+  cursor: pointer;
+  margin-top: 15px;
+}
+
+.modal .close {
+  background-color: red;
+  margin-top: 7px;
 }
 </style>
